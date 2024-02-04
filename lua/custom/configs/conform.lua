d5e08a80a7521a941dc943d69c9444ab7765d3e7
Kenny16372 <kenny16372@gmail.com>
2024-02-04 20:27:43 +0100

    feat: Format typescript files using prettier

--- lua/custom/configs/conform.lua
+++ lua/custom/configs/conform.lua
@@ -1,25 +1,26 @@
 --type conform.options
 local options = {
-	lsp_fallback = true,
+  lsp_fallback = true,
 
-	formatters_by_ft = {
-		lua = { "stylua" },
+  formatters_by_ft = {
+    lua = { "stylua" },
 
-		javascript = { "prettier" },
-		css = { "prettier" },
-		html = { "prettier" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
 
-		sh = { "shfmt" },
-	},
+    sh = { "shfmt" },
+  },
 
   -- adding same formatter for multiple filetypes can look too much work for some
   -- instead of the above code you could just use a loop! the config is just a table after all!
 
-	format_on_save = {
-	  -- These options will be passed to conform.format()
-	  timeout_ms = 500,
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 1000,
     lsp_fallback = true,
-	},
+  },
 }
 
 require("conform").setup(options)
